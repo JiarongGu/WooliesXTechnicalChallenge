87e322e8f7a1e910ca0dd7f123a081f337318571
Jiarong Gu <jiaronggu@hotmail.com>
2018-08-05 20:13:31 +1000

    added ResourceService

---
+++ WooliesXTechnicalChallenge/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WooliesXTechnicalChallenge.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static T Get<T>(this HttpClient httpClient, string requestUri) {
+            var result = httpClient.GetAsync(requestUri).Result;
+
+            if (!result.IsSuccessStatusCode)
+                throw new HttpRequestException($"external request failed./n{result.ReasonPhrase}");
+
+            var json = result.Content.ReadAsStringAsync().Result;
+            var objects = JsonConvert.DeserializeObject<T>(json);
+
+            return objects;
+        }
+    }
+}
---
+++ WooliesXTechnicalChallenge/Services/ResourceService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using WooliesXTechnicalChallenge.Enums;
+using WooliesXTechnicalChallenge.Extensions;
+using WooliesXTechnicalChallenge.Models;
+using WooliesXTechnicalChallenge.Options;
+
+namespace WooliesXTechnicalChallenge.Services
+{
+    public class ResourceService: IResourceService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IQueryBuilderService _queryBuilderService;
+        private readonly ResourceApiSettings _apiSettings;
+        private readonly QuerySettings _querySettings;
+        private readonly TesterSettings _testerSettings;
+
+        public ResourceService(
+            IHttpClientFactory httpClientFactory, 
+            IQueryBuilderService queryBuilderService, 
+            IOptions<ResourceApiSettings> apiSettings,
+            IOptions<QuerySettings> querySettings,
+            IOptions<TesterSettings> testerSettings
+            )
+        {
+            _httpClientFactory = httpClientFactory;
+            _queryBuilderService = queryBuilderService;
+            _apiSettings = apiSettings.Value;
+            _querySettings = querySettings.Value;
+            _testerSettings = testerSettings.Value;
+        }
+        
+        public ShopperHistory GetShopperHisotry() {
+            return GetResourceHttpClient().Get<ShopperHistory>($"{_apiSettings.ShooperHistoryUri}?{GetTokenQuery()}");
+        }
+
+        public IEnumerable<Product> GetProducts() {
+            return GetResourceHttpClient().Get<IEnumerable<Product>>($"{_apiSettings.ProductsUri}?{GetTokenQuery()}");
+        }
+
+        private HttpClient GetResourceHttpClient() {
+            return _httpClientFactory.CreateClient(_apiSettings.Key);
+        }
+
+        private string GetTokenQuery() {
+            var tokenParameter = new Dictionary<string, string>();
+            tokenParameter[_querySettings.Token] = _testerSettings.Token;
+
+            return _queryBuilderService.BuildQuery(tokenParameter);
+        }
+    }
+}
