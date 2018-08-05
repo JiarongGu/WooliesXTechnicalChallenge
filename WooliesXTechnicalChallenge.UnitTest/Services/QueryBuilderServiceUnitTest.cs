48060c7217283019667bb85719323382ee9a6675
Jiarong Gu <jiaronggu@hotmail.com>
2018-08-05 19:30:52 +1000

    added QueryBuilderService and QueryBuilderServiceUnitTest

---
+++ WooliesXTechnicalChallenge.UnitTest/Services/QueryBuilderServiceUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WooliesXTechnicalChallenge.Services;
+using Xunit;
+
+namespace WooliesXTechnicalChallenge.UnitTest.Services
+{
+    public class QueryBuilderServiceUnitTest
+    {
+        [Fact]
+        public void Build_Should_Handle_Null()
+        {
+            var build = new QueryBuilderService();
+            var query = build.BuildQuery(null);
+
+            Assert.Equal(string.Empty, query);
+        }
+
+        [Fact]
+        public void Build_Should_Handle_Single_Parameter() {
+            var build = new QueryBuilderService();
+
+            var parameters = new Dictionary<string, string>();
+            parameters["token"] = "c72813cd-9879-436a-9a6d-9497e2cf5771";
+
+            var query = build.BuildQuery(parameters);
+
+            Assert.Equal("token=c72813cd-9879-436a-9a6d-9497e2cf5771", query);
+        }
+
+        [Fact]
+        public void Build_Should_Handle_Multiple_Parameters()
+        {
+            var build = new QueryBuilderService();
+
+            var parameters = new Dictionary<string, string>();
+            parameters["token"] = "c72813cd-9879-436a-9a6d-9497e2cf5771";
+            parameters["customerId"] = "1";
+
+            var query = build.BuildQuery(parameters);
+
+            Assert.Equal("token=c72813cd-9879-436a-9a6d-9497e2cf5771&customerId=1", query);
+        }
+    }
+}
---
+++ WooliesXTechnicalChallenge/Services/QueryBuilderService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WooliesXTechnicalChallenge.Services
+{
+    public class QueryBuilderService: IQueryBuilderService
+    {
+        public string BuildQuery(IDictionary<string, string> keyValues) {
+            if (keyValues == null)
+                return string.Empty;
+
+            var query = HttpUtility.ParseQueryString(string.Empty);
+            
+            foreach (var keyValue in keyValues) {
+                query[keyValue.Key] = keyValue.Value;
+            }
+            return query.ToString();
+        }
+    }
+}
